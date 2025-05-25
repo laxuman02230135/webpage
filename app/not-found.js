89b96e12d7a0d65a90a56905ee529ed02df4e6f8
LaxuMan3324 <02230135.cst@rub.edu.bt>
2025-05-26 00:32:31 +0600

    change

--- app/not-found.js
+++ app/not-found.js
@@ -1,6 +1,9 @@
-'use client';
-
 import Link from 'next/link';
+
+export const metadata = {
+  title: '404 - Page Not Found',
+  description: 'The page you are looking for does not exist.',
+};
 
 export default function NotFound() {
   return (
